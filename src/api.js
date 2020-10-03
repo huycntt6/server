c30172a92c27cc7f914d6e20a674d4e9456d4bbe
Hoang Van Huy <huynoone@gmail.com>
2020-10-03 22:20:51 +0700

    cm

--- src/api.js
+++ src/api.js
@@ -1,16 +1,48 @@
-const express = require("express");
+//import dotenv
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const port = process.env.PORT || 8080;
+
 const serverless = require("serverless-http");
 
-const app = express();
-const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({
-    hello: "hi!"
-  });
+//connect to DB
+mongoose.connect(
+    process.env.MONGO_URI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true 
+    }
+)
+.then(()=>console.log('Đã kết nối DB!'))
+.catch(err=>console.log('Lỗi Kết nối DB!')
+);
+
+
+//import Routes
+const authRoute = require('../routes/auth');
+//postroutes
+const postRoute = require('../routes/posts');
+
+//Middlewares
+ 
+app.use(express.json());
+
+//Route Middlewares
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
 });
 
-app.use(`/.netlify/functions/api`, router);
+app.use('/.netlify/functions/api/user', authRoute);
+app.use('/.netlify/functions/api/posts', postRoute);
+//app.listen(port, ()=> console.log('Server đang hoạt động! port => ' + port));
 
 module.exports = app;
 module.exports.handler = serverless(app);
